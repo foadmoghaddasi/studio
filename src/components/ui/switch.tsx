580ea86145cbda154bef548fa499d69670b01db8
Foad Moghadasi <moghadasi.foad@gmail.com>
2025-05-26 22:03:17 +0000

    استایل همه سوییچ ها در حالتی که خاموش هستن این جوریه background #00BFA6 opacity 10% strok #00BFA6 2px و اندازه دایره داخلی 16 در 16 پیکسل هست

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -12,7 +12,9 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-8 w-[52px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-secondary",
+      "peer inline-flex h-8 w-[52px] shrink-0 cursor-pointer items-center rounded-full border-2 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
+      "data-[state=checked]:bg-primary data-[state=checked]:border-primary",
+      "data-[state=unchecked]:bg-primary/10 data-[state=unchecked]:border-primary",
       className
     )}
     {...props}
@@ -20,7 +22,8 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-6 w-6 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=unchecked]:translate-x-0 data-[state=checked]:ltr:translate-x-6 data-[state=checked]:rtl:-translate-x-6"
+        "pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=unchecked]:translate-x-0",
+        "data-[state=checked]:ltr:translate-x-8 data-[state=checked]:rtl:-translate-x-8"
       )}
     />
   </SwitchPrimitives.Root>
