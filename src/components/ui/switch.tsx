7dcf9d9f6d1b20264fdea4d32a1bc3ad71dcde41
Foad Moghadasi <moghadasi.foad@gmail.com>
2025-05-26 22:11:06 +0000

    ببین توی دیزاین فیگما اون دایره داخلی در حالت روشن از بالا و پایین و کنار 4 پیکسل فاصله داره و اندازش 24 در 24 هستش

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -12,7 +12,7 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-8 w-[52px] shrink-0 cursor-pointer items-center rounded-full border-2 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
+      "peer inline-flex h-9 w-[52px] shrink-0 cursor-pointer items-center rounded-full border-2 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
       // Root background and border for checked state
       "data-[state=checked]:bg-primary data-[state=checked]:border-primary",
       // Root background and border for unchecked state
@@ -32,7 +32,7 @@ const Switch = React.forwardRef<
         // Checked state: 24x24px, background color (white), 4px offset from edge
         "data-[state=checked]:h-6 data-[state=checked]:w-6",
         "data-[state=checked]:bg-background",
-        "data-[state=checked]:ltr:translate-x-5 data-[state=checked]:rtl:-translate-x-5" // 20px offset (1.25rem)
+        "data-[state=checked]:ltr:translate-x-5 data-[state=checked]:rtl:-translate-x-5" // 20px offset (1.25rem) from left for LTR
       )}
     />
   </SwitchPrimitives.Root>
@@ -40,3 +40,4 @@ const Switch = React.forwardRef<
 Switch.displayName = SwitchPrimitives.Root.displayName
 
 export { Switch }
+
